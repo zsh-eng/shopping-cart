a030fa191658da164e16a191cc10fb19da29f412
sheng <49238630+zsh-eng@users.noreply.github.com>
2022-06-27 17:21:33 +0800

    style: Add animation for cart

--- src/Cart.js
+++ src/Cart.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useEffect } from "react"
 import CartItem from "./CartItem"
+import "animate.css"
 
 const Cart = ({ products, onCloseCart, changeQty }) => {
 	const total = products.reduce(
@@ -7,16 +8,39 @@ const Cart = ({ products, onCloseCart, changeQty }) => {
 		0
 	)
 
+	useEffect(() => {
+		const cartBg = document.getElementById("cart-bg")
+		const cartContainer = document.getElementById("cart-container")
+
+		const handleCloseCart = () => {
+			cartContainer.addEventListener("animationend", onCloseCart, {
+				once: true,
+			})
+			cartContainer.classList.remove("animate__slideInRight")
+			cartContainer.classList.add("animate__slideOutRight")
+		}
+
+		cartBg.addEventListener("click", handleCloseCart)
+
+		return () => {
+			cartBg.removeEventListener("click", handleCloseCart)
+		}
+	}, [])
+
+
 	return (
-		<div className="h-screen w-screen absolute flex">
+		<div className="h-screen w-screen fixed flex justify-end">
 			<div
-				className="w-2/3 bg-black opacity-75"
-				onClick={() => onCloseCart()}
+				className="z-10 fixed top-0 left-0 h-full w-full bg-black opacity-75"
+				id="cart-bg"
 			></div>
-			<div className="w-1/3 bg-white px-8 py-16 pt-8 text-gray-700 grid grid-rows-[max-content_1fr_max-content]">
+			<div
+				className="z-20 w-1/3 bg-white px-8 py-16 pt-8 text-gray-700 grid grid-rows-[max-content_1fr_max-content]  animate__animated animate__faster animate__slideInRight"
+				id="cart-container"
+			>
 				<h1 className="text-5xl font-bold flex justify-center mb-4">Cart</h1>
 
-				<div className="flex flex-col gap-4 overflow-auto scroll-smooth">
+				<div className="mt-4 mb-8 flex flex-col gap-4 overflow-auto scroll-smooth">
 					{products.map(({ image, name, price, quantity }) => (
 						<CartItem
 							key={name}
